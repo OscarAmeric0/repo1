5b119f0fe2d1cf861d91c28857d427f444238485
oscaramerico <i201511042@cibertec.edu.pe>
2022-02-26 15:38:48 -0500

    comienzo del proyecto

---
+++ hola_mundo.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "hola mundo";
+
+echo "hola mundo 2";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">	
+<title>documentos</title>
+</head>
+<body>
+<h1>hola mundo</h1>
+</body>
+</html>
